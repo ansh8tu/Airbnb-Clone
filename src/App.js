88deeeee4718ff7a49e21bc1c7ed2472e9fd81bd
ansh8tu <anshu8tu@gmail.com>
2022-06-20 05:26:54 +0530

    Added props for Katie Card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import MidSection from "./components/MidSection"
-import FooterSection from "./components/FooterSection"
+import Card from "./components/Card"
+import KatieImg from "./images/katie-zaferes.png"
 
 export default function App() {
     return (
         <>
             <Navbar />
             <MidSection />
-            <FooterSection />
+            <Card 
+                img={KatieImg}
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
         </>
     )
 }
