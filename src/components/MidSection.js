bffb4682232aa82e430908dd8c8dea43c04c094e
ansh8tu <anshu8tu@gmail.com>
2022-06-20 10:23:02 +0530

    Fixed bugs due to moving images to public dir!

--- src/components/MidSection.js
+++ src/components/MidSection.js
@@ -1,11 +1,10 @@
 import React from "react"
-import MidSectionImg from "../images/mid-section-img.png"
 
 export default function MidSection () {
     return (
         <>
             <div className="mid--div">
-                    <img className="mid-sec-img" src={MidSectionImg}  alt="about-airbnb"></img>
+                    <img className="mid-sec-img" src={"./images/mid-section-img.png"}  alt="about-airbnb"></img>
                     <h1 className="mid--heading">Online Experiences</h1>
                     <p className="mid--para">Join unique interactive activities led by 
                     one-of-a-kind hosts—all without leaving home.</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from "react"
-import AirbnbLogo from "../images/airbnb.png"
+
 export default function Navbar() {
     return (
         <>
             <nav>
-                <img className="nav--img" src = {AirbnbLogo} width="40px" alt="Airbnb"></img>
+                <img className="nav--img" src = {"./images/airbnb.png"} width="40px" alt="Airbnb"></img>
                 <h3 className="nav--title">airbnb</h3>
             </nav>
         </>
